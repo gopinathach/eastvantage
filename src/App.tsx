074020fdb295f1240bddada1e1ebba163519a015
gopinathach <gopinathm1708@gmail.com>
2022-10-28 21:59:58 +0530

    Task Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import PeopleCard from "./comps/peopleCard/PeopleCard";
 
 function App() {
+  const [apiData, setApiData] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  var config = {
+    method: "get",
+    url: "https://randomuser.me/api",
+  };
+
+  async function getData() {
+    var userData = await axios(config);
+    console.log("userData", userData.data.results);
+    setApiData(userData.data.results);
+    localStorage.setItem("userData", JSON.stringify(apiData));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App d-flex justify-content-center align-items-center container">
+      {apiData.map((curr: any, i) => (
+        <div key={i.toString()}>
+          <PeopleCard
+            name={`${curr.name.title}. ${curr.name.first} ${curr.name.last}`}
+            email={curr.email}
+            age={`Age: ${curr.dob.age}`}
+            department={curr.cell}
+            phone={curr.phone}
+            address={`${curr.location.city}, ${curr.location.country}.`}
+            initial={
+              curr.name.first && curr.name.last ? (
+                curr.name.first.split("")[0] + curr.name.last.split("")[0]
+              ) : (
+                <i className="fas fa-user-alt"></i>
+              )
+            }
+            imageUrl={curr.picture.large}
+            getData={getData}
+          />
+        </div>
+      ))}
     </div>
   );
 }
---
+++ src/comps/peopleCard/PeopleCard.tsx
@@ -0,0 +1,133 @@
+import { Tooltip } from "antd";
+import React, { useState } from "react";
+import styles from "./peopleContactCard.module.sass";
+import content from "./assets/content.svg";
+import map from "./assets/map.svg";
+import phones from "./assets/phone.svg";
+import mail from "./assets/mail.svg";
+
+export default function PeopleCard({
+  name,
+  email,
+  age,
+  department,
+  phone,
+  address,
+  initial,
+  imageUrl,
+  getData,
+}: any) {
+  const [copySuccess, setCopySuccess] = useState<string>("");
+  const [copyId, setCopyId] = useState<Number>(0);
+
+  const copyToClipBoard = async (copyMe: any) => {
+    console.log(copyMe);
+    try {
+      await navigator.clipboard.writeText(copyMe);
+      setCopySuccess("Copied!");
+    } catch (err) {
+      setCopySuccess("Failed to copy!");
+    }
+    setTimeout(() => {
+      setCopySuccess("");
+    }, 1000);
+  };
+
+  return (
+    <div className={`${styles.profileBox} overflow-hidden`}>
+      <div className="p-3">
+        <div className={`d-flex`}>
+          <div
+            className={`text-white d-flex justify-content-center align-items-center rounded-circle overflow-hidden ${styles.profilePic}`}
+            style={{
+              backgroundColor: `rgb(${Math.floor(
+                Math.random() * 256
+              )},${Math.floor(Math.random() * 256)},${Math.floor(
+                Math.random() * 256
+              )})`,
+            }}
+          >
+            {imageUrl ? (
+              <img src={imageUrl} className={`${styles.profilePic}`} alt="" />
+            ) : (
+              initial
+            )}
+          </div>
+          <div className="px-3">
+            <div className={`${styles.profileFont}`}>{name}</div>
+            <div className={`${styles.profileGray} py-1`}>{age}</div>
+            <div className={`${styles.profileFontSmall} pb-1`}>
+              {department}
+            </div>
+          </div>
+        </div>
+        <div className={`${styles.profileFont} pt-4 pb-2`}>
+          Contact Information
+        </div>
+
+        <div
+          className={`d-flex justify-content-between align-items-center w-100`}
+        >
+          <div className={`d-flex align-items-center w-100`}>
+            <img src={mail} width={`13px`} alt="" />{" "}
+            <div className={`ps-2 ${styles.profileFontSmall}`}>{email}</div>
+          </div>
+          <div
+            style={{ cursor: "pointer", color: "red" }}
+            onClick={() => {
+              copyToClipBoard(email);
+              setCopyId(0);
+            }}
+          >
+            <img src={content} width={`13px`} alt="" />
+            <div className={styles.tooltip}>
+              {copySuccess === `Copied!` && copyId == 0 && copySuccess}
+            </div>
+          </div>
+        </div>
+        <div className="d-flex justify-content-between align-items-center w-100 py-2">
+          <div className={`d-flex align-items-center w-100`}>
+            <img src={phones} width={`13px`} alt="" />
+            <div className={`ps-2 ${styles.profileFontSmall}`}>{phone}</div>
+          </div>
+          <div
+            style={{ cursor: "pointer" }}
+            onClick={() => {
+              copyToClipBoard(phone);
+              setCopyId(1);
+            }}
+          >
+            <img src={content} width={`13px`} alt="" />
+            <div className={styles.tooltip}>
+              {copySuccess === `Copied!` && copyId == 1 && copySuccess}
+            </div>
+          </div>
+        </div>
+        <div className="d-flex justify-content-between align-items-center w-100">
+          <div className={`d-flex align-items-center w-100`}>
+            <img src={map} width={`13px`} alt="" />{" "}
+            <div className={`ps-2 ${styles.profileFontSmall}`}>{address}</div>
+          </div>
+          <div
+            style={{ cursor: "pointer" }}
+            onClick={() => {
+              copyToClipBoard(address);
+              setCopyId(2);
+            }}
+          >
+            <img src={content} width={`13px`} alt="" />
+            <div className={styles.tooltip}>
+              {copySuccess === `Copied!` && copyId == 2 && copySuccess}
+            </div>
+          </div>
+        </div>
+      </div>
+      <button
+        className={`d-flex justify-content-center align-items-center w-100 ${styles.profileFetchNew}`}
+        onClick={getData}
+      >
+        Fetch New
+      </button>
+    </div>
+  );
+}
